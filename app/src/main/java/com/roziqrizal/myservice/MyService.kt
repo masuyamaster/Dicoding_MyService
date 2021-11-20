ea111bd8228bc7fc751a12019fa9e6a0856e7232
masuyamaster <roziq@masuya.id>
2021-11-20 10:42:45 +0700

    stop service otomatis setelah dijalankan

--- app/src/main/java/com/roziqrizal/myservice/MyService.kt
+++ app/src/main/java/com/roziqrizal/myservice/MyService.kt
@@ -5,8 +5,16 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
 class MyService : Service() {
+
+    companion object {
+        internal val TAG = MyService::class.java.simpleName
+    }
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
@@ -14,6 +22,17 @@ class MyService : Service() {
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         Log.d(TAG, "Service dijalankan...");
+        serviceScope.launch {
+            delay(3000)
+            stopSelf()
+            Log.d(TAG, "Service dihentikan")
+        }
         return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel()
+        Log.d(TAG, "onDestroy: ")
     }
 }
